2134451d3ad07093a08873b66001faef49737344
Daniel Jorge <daniel.souza@seplag.ce.gov.br>
2018-07-24 16:39:34 -0300

    adding cache with spring

---
+++ src/main/java/br/com/casadocodigo/loja/conf/JsonviewResolver.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package br.com.casadocodigo.loja.conf;
+
+import java.util.Locale;
+
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+/**
+ * @author daniel.souza
+ *
+ */
+public class JsonviewResolver implements ViewResolver {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.web.servlet.ViewResolver#resolveViewName(java.lang.String, java.util.Locale)
+	 */
+	@Override
+	public View resolveViewName(String viewName, Locale locale) throws Exception {
+		MappingJackson2JsonView jsonView = new MappingJackson2JsonView();
+		jsonView.setPrettyPrint(true);
+		
+		return jsonView;
+	}
+
+}
